024b250c352a9b572a1837b737c148b54e350f49
David Cornelius <davidcornelius15@gmail.com>
2017-08-03 11:20:38 +0700

    Alert ok and alert with option and using navigation for button back

--- UIAlertController/ViewController.swift
+++ UIAlertController/ViewController.swift
@@ -10,16 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        
     }
 
 
+    func createAlert(title:String, message:String, alertType:String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        
+        switch alertType {
+        case "OPTION":
+            alert.addAction(UIAlertAction(title: "Yes", style: UIAlertActionStyle.default, handler: { (action) in
+                alert.dismiss(animated: true, completion: nil)
+                self.performSegue(withIdentifier: "segue", sender: self)
+            
+            }))
+            
+            
+            alert.addAction(UIAlertAction(title: "No", style: UIAlertActionStyle.default, handler: { (action) in
+                alert.dismiss(animated: true, completion: nil)
+            }))
+            self.present(alert, animated: true, completion: nil)
+            print("OPTIONS")
+            break
+            
+        case "OK":
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: { (action) in
+                alert.dismiss(animated: true, completion: nil)
+            }))
+            
+            self.present(alert, animated: true, completion: nil)
+            break
+        default:
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: { (action) in
+                alert.dismiss(animated: true, completion: nil)
+            }))
+            self.present(alert, animated: true, completion: nil)
+           
+        }
+    }
+    
+    @IBAction func alertOk(_ sender: UIButton) {
+        createAlert(title: "Warning", message: "Pastikan Anda telah mengisikan Email dan Password dengan benar!",alertType: "OK")
+     
+    }
+    @IBAction func alertOption(_ sender: UIButton) {
+        createAlert(title: "Konfirmasi", message: "Apakah Anda ingin memesan produk ini?", alertType: "OPTION")
+        
+    }
+    
 }
 
